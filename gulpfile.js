861dafc72648b1a5df9548c5510a61d2012f6521
egle <egle@bluemantis.com>
2021-08-18 22:41:50 +0100

    update gulpfile and build

--- gulpfile.js
+++ gulpfile.js
@@ -56,9 +56,9 @@ gulp.task('useref', function(){
 gulp.task('images', function(){
   return gulp.src('app/images/**/*.+(png|jpg|gif|svg)')
     // Caching images that ran through imagemin
-    .pipe(cache(imagemin({
-      interlaced: true
-    })))
+    // .pipe(cache(imagemin({
+    //   interlaced: true
+    // })))
     .pipe(gulp.dest('dist/images'))
 });
 
